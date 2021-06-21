e6ed236f874751e4ab7a88631365b055bf0e47d1
alifdevz <alifm.devz@gmail.com>
2021-06-21 22:06:48 +0800

    Add conditions code to compare two variables

---
+++ conditions/conditions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 9;
+    int y = 3;
+
+    if (x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
+}
