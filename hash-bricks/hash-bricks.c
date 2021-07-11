8889dc31087b8990bd0951a8517f324683004f9e
alifdevz <alifm.devz@gmail.com>
2021-07-11 23:10:46 +0800

    Added hash-bricks.c

---
+++ hash-bricks/hash-bricks.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
