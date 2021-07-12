cbb33c442d76a79ccc90e9feea2759ab0d41f606
alifdevz <alifm.devz@gmail.com>
2021-07-12 11:29:01 +0800

    Added full-pyramid.c

---
+++ full-pyramid/full-pyramid.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 5;
+
+    int x = 5;
+    for (int i = 0; i < n; i++)
+    {
+        // Print the right-aligned pyramid
+        for (int j = x; j > 1; j--)
+        {
+            printf(" ");
+        }
+        x--;
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+
+        // Separator
+        printf(" ");
+
+        // Print the left-aligned pyramid
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
