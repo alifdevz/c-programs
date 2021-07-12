cfa8076b420de750b5a0e2c8ee6c65e545d67d5e
alifdevz <alifm.devz@gmail.com>
2021-07-12 10:09:46 +0800

    Print dots (.)

--- right-aligned-pyramids/right-aligned-pyramids.c
+++ right-aligned-pyramids/right-aligned-pyramids.c
@@ -2,5 +2,13 @@
 
 int main(void)
 {
-    
+    int n = 5;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = n; j > 1; j--)
+        {
+            printf(".");
+        }
+        n--;
+    }
 }
